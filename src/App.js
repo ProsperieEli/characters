d5ecd5b39068fef07e42504db3c6442c84aea32a
ProsperieEli <ElijahProsperie@gmail.com>
2021-11-30 17:47:29 -0500

    Picker is finished and works on live site

--- src/App.js
+++ src/App.js
@@ -2,18 +2,34 @@ import "./App.css";
 import { useState } from "react";
 import Picker from "./Components/Picker";
 import Character from "./Components/Character";
+import Display from "./Components/Display";
 
 function App() {
   const [head, setHead] = useState("");
   const [middle, setMiddle] = useState("");
   const [bottom, setBottom] = useState("");
-  const [input, setInput] = useState("");
-  const [catchPhrase, setCatchPhrase] = useState("");
+  const [newCatchPhrase, setNewCatchPhrase] = useState("");
+  const [catchPhrase, setCatchPhrase] = useState([]);
+
+  const handleClick = () => {
+    setCatchPhrase((prevState) => [...prevState, newCatchPhrase]);
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <Picker />
+        <Picker
+          head={head}
+          onHeadChange={setHead}
+          middle={middle}
+          onMiddleChange={setMiddle}
+          bottom={bottom}
+          onBottomChange={setBottom}
+          newCatchPhrase={newCatchPhrase}
+          setCatchphrase={setNewCatchPhrase}
+          handleClick={handleClick}
+        />
+        <Display catchphrase={catchPhrase} />
         <Character />
       </header>
     </div>
--- src/Components/Picker.jsx
+++ src/Components/Picker.jsx
@@ -7,7 +7,7 @@ export default function Picker({
   onHeadChange,
   onMiddleChange,
   onBottomChange,
-  NewatchPhrase,
+  newCatchPhrase,
   setCatchphrase,
   handleClick,
 }) {
@@ -18,11 +18,40 @@ export default function Picker({
     <div>
       <label>
         Head
-        <select
-          value={headPic}
-          onChange={(e) => onHeadChange(e.target.value)}
-        ></select>
+        <select value={head} onChange={(e) => onHeadChange(e.target.value)}>
+          {headPic.map((heads) => (
+            <option key={heads}>{heads}</option>
+          ))}
+        </select>
       </label>
+      <label>
+        middle
+        <select value={middle} onChange={(e) => onMiddleChange(e.target.value)}>
+          {middlePic.map((middles) => (
+            <option key={middles}>{middles}</option>
+          ))}
+        </select>
+      </label>
+      <label>
+        bottom
+        <select value={bottom} onChange={(e) => onBottomChange(e.target.value)}>
+          {bottomPic.map((bottoms) => (
+            <option key={bottoms}>{bottoms}</option>
+          ))}
+        </select>
+      </label>
+      <label>
+        New Catchphase
+        <input
+          type="text"
+          value={newCatchPhrase}
+          onChange={(e) => setCatchphrase(e.target.value)}
+        />
+      </label>
+
+      <button type="button" onClick={handleClick}>
+        Add Catchphrase!
+      </button>
     </div>
   );
 }
